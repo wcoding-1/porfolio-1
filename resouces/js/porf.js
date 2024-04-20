c739cf7d8245e0a2f7c53ab1a23f54bfb20d596c
wcoding <papa5944@gmail.com>
2024-04-20 23:56:27 +0900

    reduce the duplicate code in ja file

--- resouces/js/porf.js
+++ resouces/js/porf.js
@@ -8,55 +8,14 @@ function renderNav(nav){
     
         // console.log(text)
     for(let i = 0; i < nav.length; i++){
-        console.log(nav[i])
-        if(nav[i] == 'About Me'){
-            let li = document.createElement('li');
+
+        let li = document.createElement('li');
             let anchor = document.createElement('a');
-            li.setAttribute('class', nav[i])
+            li.setAttribute('class', nav[i]);
             anchor.setAttribute('href', '#');
             anchor.innerHTML = nav[i];
-            ul.appendChild(li)
-            li.appendChild(anchor)
-        
-        }else if(nav[i] == 'Projects'){
-            let li = document.createElement('li');
-            let anchor = document.createElement('a');
-            li.setAttribute('class', nav[i])
-            anchor.setAttribute('href', '#');
-            anchor.innerHTML = nav[i];
-            ul.appendChild(li)
-            li.appendChild(anchor)
-        
-        }else if(nav[i] == 'Skills'){
-            let li = document.createElement('li');
-            let anchor = document.createElement('a');
-            li.setAttribute('class', nav[i])
-            anchor.setAttribute('href', '#');
-            anchor.innerHTML = nav[i];
-            ul.appendChild(li)
-            li.appendChild(anchor)
-    
-        }else if(nav[i] == 'Contact'){
-            let li = document.createElement('li');
-            let anchor = document.createElement('a');
-            li.setAttribute('class', nav[i])
-            anchor.setAttribute('href', '#');
-            anchor.innerHTML = nav[i];
-            ul.appendChild(li)
-            li.appendChild(anchor)
-    
-        
-        }else if(nav[i] == 'To Do'){
-           
-            let li = document.createElement('li');
-            let anchor = document.createElement('a');
-            li.setAttribute('class', nav[i])
-            anchor.setAttribute('href', '#');
-            anchor.innerHTML = nav[i];
-            ul.appendChild(li)
-            li.appendChild(anchor)
-    
-        }
+            ul.appendChild(li);
+            li.appendChild(anchor);
    
     }
    
